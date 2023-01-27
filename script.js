5aba937824f9dbf5a0144cdd8c6dd876c0102f3b
Raunaq Singh <>
2023-01-27 01:49:05 -0600

    fixed the javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let htmlText = document.getElementById("pog").innerHTML
 
-for(i = 0; i<5; i++){
-    htmlText = `<p>${i}</p>`
+for (i = 0; i < 5; i++) {
+    document.getElementById("pog").innerHTML += `<p>${i + 1}</p>`
 }
